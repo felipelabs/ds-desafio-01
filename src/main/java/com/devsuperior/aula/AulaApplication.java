0bfc037f518e3979bfeb352885b332f39a130535
Felipe Neves <felipelabs@gmail.com>
2024-03-05 21:17:29 -0300

    Organizando identação

--- src/main/java/com/devsuperior/aula/AulaApplication.java
+++ src/main/java/com/devsuperior/aula/AulaApplication.java
@@ -22,26 +22,25 @@ public class AulaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
-
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);		
-		
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Digite o código do pedido:");
 		int codigo = sc.nextInt();
 		System.out.println("Digite o valor do pedido:");
 		double valor = sc.nextDouble();
 		System.out.println("Digite o valor do desconto:");
 		double desconto = sc.nextDouble();
-		
+
 		Order order = new Order(codigo, valor, desconto);
-		
+
 		System.out.println("Código do pedido: " + order.getCode());
 		System.out.printf("Valor Total: R$ %.2f", orderService.total(order) + shippingService.shipping(order));
-		
+
 		sc.close();
 	}
 
